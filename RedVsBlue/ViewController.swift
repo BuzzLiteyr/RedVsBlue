e1a7f8617e3e98f28cf6d23c5c61d1ae2d7575ec
Michel Besnard <macmyjl@Michel-Besnards-MacBook.local>
2016-05-23 14:30:03 -0300

    Initial RedVsBlue repository setup

--- RedVsBlue/ViewController.swift
+++ RedVsBlue/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BlueBeGone: UIButton!
+    @IBOutlet weak var RedBeGone: UIButton!
+    @IBOutlet weak var BringThemBack: UIButton!
+    @IBOutlet weak var Red: UIImageView!
+    @IBOutlet weak var Blue: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func RedGone(sender: AnyObject) {
+        Red.hidden = true
+        RedBeGone.hidden = true
+        
+    }
+    @IBAction func BlueGone(sender: AnyObject) {
+        Blue.hidden = true
+        BlueBeGone.hidden = true
+        
+    }
 
+    @IBAction func AllBack(sender: AnyObject) {
+        Red.hidden = false
+        RedBeGone.hidden = false
+        Blue.hidden = false
+        BlueBeGone.hidden = false
+    }
 }
 
